11c64c8dd23ee1b29a1ab953ed12e3885239cb63
YaroslavBoldyshev <yaroslavboldushev@gmail.com>
2024-08-21 21:10:52 +0200

    home page

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,34 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ComponentsAnnouncement extends Schema.Component {
+  collectionName: 'components_components_announcements';
+  info: {
+    displayName: 'Announcement';
+    icon: 'envelop';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    text: Attribute.Text & Attribute.Required;
+    image: Attribute.Media & Attribute.Required;
+    socialLinkls: Attribute.Component<'helpers.social-media', true>;
+  };
+}
+
+export interface ComponentsBanner extends Schema.Component {
+  collectionName: 'components_helpers_banners';
+  info: {
+    displayName: 'Banner';
+    icon: 'picture';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    buttonText: Attribute.String & Attribute.Required;
+    bannerImage: Attribute.Media & Attribute.Required;
+  };
+}
+
 export interface HeaderNavigationMenu extends Schema.Component {
   collectionName: 'components_header_navigation_menus';
   info: {
@@ -25,11 +54,27 @@ export interface HelpersPosition extends Schema.Component {
   };
 }
 
+export interface HelpersSocialMedia extends Schema.Component {
+  collectionName: 'components_helpers_social_medias';
+  info: {
+    displayName: 'Social Media';
+    icon: 'twitter';
+  };
+  attributes: {
+    socialLink: Attribute.String & Attribute.Required;
+    socialMediaName: Attribute.String & Attribute.Required;
+    icon: Attribute.Media & Attribute.Required;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'components.announcement': ComponentsAnnouncement;
+      'components.banner': ComponentsBanner;
       'header.navigation-menu': HeaderNavigationMenu;
       'helpers.position': HelpersPosition;
+      'helpers.social-media': HelpersSocialMedia;
     }
   }
 }
