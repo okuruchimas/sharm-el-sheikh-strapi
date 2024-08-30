1d949ba4c3e5e99d65d93099978c6d697f99c1bf
YaroslavBoldyshev <yaroslavboldushev@gmail.com>
2024-08-30 19:11:32 +0200

    revert middlewares.ts cchanges

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,15 +2,7 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: ['http://localhost:3000', 'https://sharm-el-sheikh.vercel.app'],
-      headers: '*',
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
