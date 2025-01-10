d93baee73939ddafbd5763d38add6e17234a1ef5
YaroslavBoldyshev <yaroslavboldushev@gmail.com>
2025-01-10 17:59:33 +0100

    remove redundant col types

--- src/api/company-promotion-card/routes/company-promotion-card.ts
+++
@@ -1,7 +0,0 @@
-/**
- * company-promotion-card router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::company-promotion-card.company-promotion-card');
--- src/api/company-promotion-card/services/company-promotion-card.ts
+++
@@ -1,7 +0,0 @@
-/**
- * company-promotion-card service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::company-promotion-card.company-promotion-card');
